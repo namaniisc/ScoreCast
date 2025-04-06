d25f396d3db0df8d94964f79ecd4c99ef80b07d1
Naman Saraswat <namaniisc@gmail.com>
2025-04-06 12:06:07 +0530

    Handle Form Submission in Flask

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        batting_team = request.form['batting_team']
+        bowling_team = request.form['bowling_team']
+        city = request.form['city']
+        current_score = request.form['current_score']
+        overs = request.form['overs']
+        wickets = request.form['wickets']
+        last_five = request.form['last_five']
+        # Process the data here
+        return f"Received data: {batting_team}, {bowling_team}, {city}, {current_score}, {overs}, {wickets}, {last_five}"
     return render_template('index.html')
 
 if __name__ == '__main__':
